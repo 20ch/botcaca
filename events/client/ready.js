ac6b11fb1072e1595801fa86d9500046dd2e7219
atomic <146490689+20ch@users.noreply.github.com>
2024-09-19 21:53:00 +0200

    Add files via upload

---
+++ events/client/ready.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const disbut = require("discord-buttons")
+const db = require("quick.db")
+
+
+
+module.exports = (client) => {
+    console.log(`- Connecté à ${client.user.username}`)
+    client.guilds.cache.map(async guild => {
+        await guild.members.fetch().catch(e => { })
+    })
+}
