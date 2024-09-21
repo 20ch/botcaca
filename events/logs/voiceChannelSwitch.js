3b595f665008babe4fa23f82304d6b5e9e35ad04
Rofl <88626207+wheresrofl@users.noreply.github.com>
2024-09-21 12:43:03 +0100

    fix(s)

--- events/logs/voiceChannelSwitch.js
+++ events/logs/voiceChannelSwitch.js
@@ -1,18 +1,25 @@
-const axios = require('axios');         
-const db = require("quick.db")
-const Discord = require("discord.js");
-const ms = require("ms")
+const db = require("quick.db");
+const { MessageEmbed } = require("discord.js");
 
-module.exports = (client, oldChannel,newChannel) => {
-    let wass = db.get(`${oldChannel.guild.id}.logvc`);
-   const color = db.get(`color_${member.guild.id}`) === null? client.config.color:db.get(`color_${member.guild.id}`)
-      const logschannel = oldChannel.guild.channels.cache.get(wass)
-      if(logschannel) logschannel.send(new Discord.MessageEmbed()
-      .setAuthor(member.user.username, member.user.displayAvatarURL({dynamic : true }))
-      .setColor(color)
-      .setDescription(`**${member}** a changer de salon vocal, il a quitter ${oldChannel.name} 
-      et a rejoint ${newChannel.name}`)
-    
-      )
+module.exports = (client, oldState, newState) => {
+    if (oldState.channel && newState.channel && oldState.channel.id !== newState.channel.id) {
+        const member = newState.member;
+        const guild = member.guild;
+        const color = db.get(`color_${guild.id}`) || client.config.color;
+        const logChannelId = db.get(`logvc_${guild.id}`);
+        const logChannel = guild.channels.cache.get(logChannelId);
 
-}
+        if (logChannel) {
+            const embed = new MessageEmbed()
+                .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true }))
+                .setColor(color)
+                .setDescription(`**${member.user.tag}** a changé de salon vocal, a quitté **${oldState.channel.name}** et a rejoint **${newState.channel.name}**`)
+                .setTimestamp();
+
+            logChannel.send(embed)
+                .catch(err => console.error('Error sending message:', err));
+        } else {
+            console.log('Log channel not found.');
+        }
+    }
+};
