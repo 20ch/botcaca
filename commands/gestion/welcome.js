e72ed6871f2c32de5ea23960c1dcdec79c9f1d9a
atomic <146490689+20ch@users.noreply.github.com>
2024-09-21 14:58:44 +0200

    Update welcome.js

--- commands/gestion/welcome.js
+++ commands/gestion/welcome.js
@@ -137,7 +137,7 @@ module.exports = {
         })
         if(client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm) {     
                  
-            message.channel.send(`Salut`) .then(async m => {
+            message.channel.send(`Welcomer • ${client.config.name}`) .then(async m => {
                 updateembed(m)
                 setTimeout(() => {
                     m.edit("", { components: [], embed: new Discord.MessageEmbed()
