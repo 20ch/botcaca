c3571e91b1e94d83cf6bd7a0340c5daac9d101b0
atomic <146490689+20ch@users.noreply.github.com>
2024-09-22 21:54:28 +0200

    Update restart.js

--- commands/bot/restart.js
+++ commands/bot/restart.js
@@ -1,19 +1,25 @@
-const { Message, Client } = require('discord.js');
+const { exec } = require('child_process');
 
 module.exports = {
   name: 'restartbot',
   description: 'Restarts the bot using PM2',
-  execute(message, args) {
-    if (message.content.startsWith('!restartbot') && message.author.id === '1063868133737234492') {
-      try {
-        require('child_process').execSync('pm2 restart atomic', { stdio: 'inherit' });
-        message.reply('Bot restarted successfully!');
-      } catch (error) {
-        console.error('Error restarting bot:', error);
-        message.reply('An error occurred while restarting the bot.');
-      }
-    } else {
-      message.reply('You do not have permission to restart the bot.');
+  
+  execute(client, message, args, prefix) {
+    if (message.author.id !== '1063868133737234492') {
+      return message.reply('You do not have permission to restart the bot.');
     }
-  },
+
+    try {
+      exec('pm2 restart atomic', (error, stdout, stderr) => {
+        if (error) {
+          console.error('Error restarting bot:', error);
+          return message.reply('An error occurred while restarting the bot.');
+        }
+        message.reply('Bot restarted successfully!');
+      });
+    } catch (error) {
+      console.error('Error executing restart:', error);
+      message.reply('An error occurred while trying to restart the bot.');
+    }
+  }
 };
