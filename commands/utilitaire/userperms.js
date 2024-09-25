983617894495a4535e65315eaf04dbf02ba74d35
atomic <146490689+20ch@users.noreply.github.com>
2024-09-25 21:11:59 +0200

    Update userperms.js

--- commands/utilitaire/userperms.js
+++ commands/utilitaire/userperms.js
@@ -3,8 +3,7 @@ const { MessageEmbed } = require("discord.js")
 module.exports = {
   name: "userperms",
   aliases: ["usrprms"],
-  category: "info",
-  description: "user's permission in the server",
+  description: "Afficher les permissions d'un utilisateur sur le serveur.",
 
   run: async(client, message, [member = '']) => {
     if(!member.match(/\d{17,19}/)) {
@@ -12,11 +11,11 @@ module.exports = {
     };
     member = await message.guild.members.fetch(member.match(/\d{17,19}/)[0]).catch(() => null);
     if(!member) {
-        return message.channel.send(`\\❌ User not found.`);
+        return message.channel.send(`\\❌ Utilisateur introuvable. Veuillez vérifier l'orthographe du nom d'utilisateur et réessayer.`);
     };
     const sp = member.permissions.serialize();
     const cp = message.channel.permissionsFor(member).serialize();
-    return message.channel.send(new MessageEmbed().setColor(member.displayColor || 'RANDOM').setTitle(`${member.displayName}'s Permissions`).setFooter(`Permissions | \©️${new Date().getFullYear()} `).setDescription(['\\📊 - This Server', '\\#️⃣ - The Current Channel', '\`\`\`properties', '📊 | #️⃣ | Permission', '========================================', `${Object.keys(sp).map(perm => [
+    return message.channel.send(new MessageEmbed().setColor(member.displayColor || 'color').setTitle(`${member.displayName}'s Permissions`).setFooter(`Permissions | \©️${new Date().getFullYear()} `).setDescription(['\\📊 - Ce serveur', '\\#️⃣ - Ce salon', '\`\`\`properties', '📊 | #️⃣ | Permission', '========================================', `${Object.keys(sp).map(perm => [
           sp[perm] ? '✔️ |' : '❌ |',
           cp[perm] ? '✔️ |' : '❌ |',
           perm.split('_').map(x => x[0] + x.slice(1).toLowerCase()).join(' ')
