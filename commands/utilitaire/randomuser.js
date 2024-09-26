4e33ab78971d3acf97f0632cc1ac4756b8e71eba
atomic <146490689+20ch@users.noreply.github.com>
2024-09-26 18:02:31 +0200

    Update randomuser.js (add perm logic)

--- commands/utilitaire/randomuser.js
+++ commands/utilitaire/randomuser.js
@@ -1,16 +1,27 @@
-const Discord = require('discord.js')
-
+const Discord = require('discord.js');
+const db = require('quick.db'); 
 module.exports = {
-    name: 'randomuser',
-    description: "Tire un membre du serveur au hasard",
+  name: 'randomuser',
+  description: "Tire un membre du serveur au hasard (nécessite la permission `randomuser`)",
 
-    run: async (client, message, args) => {
+  run: async (client, message, args) => {
+    // Winterway 
+    let perm = "";
+    message.member.roles.cache.forEach(role => {
+      if (db.get(`randomuser_${message.guild.id}_${role.id}`)) perm = true;
+    });
 
-        let guildId = message.guild.id;
-        const guild = await client.guilds.fetch(guildId);
-        const members = await guild.members.fetch();
-        const randomMember = members.random();
+    if ( // Conditional statement for permission check
+      client.config.owner.includes(message.author.id) ||
+      db.get(`ownermd_${client.user.id}_${message.author.id}`) === true ||
+      perm
+    ) {
+      let guildId = message.guild.id;
+      const guild = await client.guilds.fetch(guildId);
+      const members = await guild.members.fetch();
+      const randomMember = members.random();
 
-        message.channel.send({ content: `${randomMember}` });
+      message.channel.send({ content: `${randomMember}` });
     }
-}
+  }
+};
