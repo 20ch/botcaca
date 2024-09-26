0794570239dd0f113a8ff0ca94637eabd376e86b
atomic <146490689+20ch@users.noreply.github.com>
2024-09-26 18:11:58 +0200

    Update ratio.js (add perms logic)

--- commands/utilitaire/ratio.js
+++ commands/utilitaire/ratio.js
@@ -1,12 +1,22 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
 module.exports = {
-    name: 'ratio',
-    description: 'Ratio un membre',
+  name: 'ratio',
+  description: 'Ratio un membre',
 
-    run: async (client, message, args) => {
+  run: async (client, message, args) => {
+    await message.react('✅');
+    await message.react('❌');
 
-       message.react('✅') 
-       message.react('❌')
+    // Winterway
+    let perm = "";
+    message.member.roles.cache.forEach(role => {
+      if (db.get(`modsp_${message.guild.id}_${role.id}`)) perm = true;
+      if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = true;
+      if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true;
+    });
+
+    if (client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm || db.get(`channelpublic_${message.guild.id}_${message.channel.id}`) === true) {
     }
-}
+  }
+};
