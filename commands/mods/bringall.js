ca0db2be830caa5f1862a2bda289ea4d0026a8b5
atomic <146490689+20ch@users.noreply.github.com>
2024-09-27 20:04:55 +0200

    Update bringall.js

--- commands/mods/bringall.js
+++ commands/mods/bringall.js
@@ -1,11 +1,9 @@
 /**
- * @author larry & 20ch
- * @message i love you
+ * @author larry
+ * @message i love u
  */
-
 const Discord = require('discord.js');
 const db = require('quick.db');
-
 module.exports = {
 	name: 'bringall',
 	aliases: [],
@@ -14,30 +12,24 @@ module.exports = {
 		if (!userVoiceChannel) {
 			return message.channel.send("Vous devez être dans un salon vocal pour exécuter cette commande.");
 		}
-
 		let perm = "";
 		message.member.roles.cache.forEach(role => {
 			if (db.get(`modsp_${message.guild.id}_${role.id}`)) perm = null;
 			if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = null;
 			if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = null;
 		});
-
 		if (!client.config.owner.includes(message.author.id) &&
 			!db.get(`ownermd_${client.user.id}_${message.author.id}`) &&
 			!perm) {
 			return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande.");
 		}
-
 		if (!message.guild.me.hasPermission("MOVE_MEMBERS")) {
 			return message.channel.send("Je n'ai pas la permission de déplacer des membres.");
 		}
-
 		const voiceChannels = message.guild.channels.cache.filter(channel => channel.type === 'voice');
-
 		if (!voiceChannels.size) {
 			return message.channel.send("Il n'y a aucun salon vocal sur ce serveur.");
 		}
-
 		let membersToMove = [];
 		voiceChannels.forEach(vc => {
 			if (vc.id !== userVoiceChannel.id) {
@@ -48,11 +40,9 @@ module.exports = {
 				});
 			}
 		});
-
 		if (membersToMove.length === 0) {
 			return message.channel.send("Il n'y a aucun membre à déplacer depuis les autres salons vocaux.");
 		}
-
 		Promise.all(membersToMove.map(member => 
 			member.voice.setChannel(userVoiceChannel.id)
 				.catch(() => {
