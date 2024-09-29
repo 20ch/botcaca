0e8bc21d9eaca290dd8d13298c5ba3b12b06c784
atomic <146490689+20ch@users.noreply.github.com>
2024-09-29 16:49:56 +0200

    Update voiceChannelLeave.js

--- events/logs/voiceChannelLeave.js
+++ events/logs/voiceChannelLeave.js
@@ -2,24 +2,22 @@ const db = require("quick.db");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = (client, oldState, newState) => {
-    if (oldState.channel && !newState.channel) {
-        const member = oldState.member;
-        const guild = member.guild;
-        const color = db.get(`color_${guild.id}`) || client.config.color;
-        const logChannelId = db.get(`logvc_${guild.id}`);
-        const logChannel = guild.channels.cache.get(logChannelId);
+  if (oldState.channel && !newState.channel) {
+    const member = oldState.member;
+    const guild = member.guild;
+    const color = db.get(`color_${guild.id}`) || client.config.color;
+    const logChannelId = db.get(`logvc_${guild.id}`);
+    const logChannel = guild.channels.cache.get(logChannelId);
 
-        if (logChannel) {
-            const embed = new MessageEmbed()
-                .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true }))
-                .setColor(color)
-                .setDescription(`**${member.user.tag}** a quitté le salon **${oldState.channel.name}**`)
-                .setTimestamp();
+    if (logChannel) {
+      const embed = new MessageEmbed()
+        .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true }))
+        .setColor(color)
+        .setDescription(`**${member.user.tag}** left the voice channel **${oldState.channel.name}**`)
+        .setTimestamp();
 
-            logChannel.send(embed)
-                .catch(err => console.error('Error sending message:', err));
-        } else {
-            console.log('Log channel not found.');
-        }
+      logChannel.send(embed)
+        .catch(err => console.error('Error sending message:', err));
     }
+  }
 };
