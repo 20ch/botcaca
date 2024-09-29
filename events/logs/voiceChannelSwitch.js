7fcde5b8eb3b3f62de7c945d3d8b4b91c6277f71
atomic <146490689+20ch@users.noreply.github.com>
2024-09-29 16:50:41 +0200

    Update voiceChannelSwitch.js

--- events/logs/voiceChannelSwitch.js
+++ events/logs/voiceChannelSwitch.js
@@ -2,24 +2,23 @@ const db = require("quick.db");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = (client, oldState, newState) => {
-    if (oldState.channel && newState.channel && oldState.channel.id !== newState.channel.id) {
-        const member = newState.member;
-        const guild = member.guild;
-        const color = db.get(`color_${guild.id}`) || client.config.color;
-        const logChannelId = db.get(`logvc_${guild.id}`);
-        const logChannel = guild.channels.cache.get(logChannelId);
+  if (oldState.channel && newState.channel && oldState.channel.id !== newState.channel.id) {
+    const member = newState.member;
+    const guild = member.guild;
+    const color = db.get(`color_${guild.id}`) || client.config.color;
+    const logChannelId = db.get(`logvc_${guild.id}`);
+    const logChannel = guild.channels.cache.get(logChannelId);
 
-        if (logChannel) {
-            const embed = new MessageEmbed()
-                .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true }))
-                .setColor(color)
-                .setDescription(`**${member.user.tag}** a changé de salon vocal, a quitté **${oldState.channel.name}** et a rejoint **${newState.channel.name}**`)
-                .setTimestamp();
+    // Check for log channel existence before sending message
+    if (logChannel) {
+      const embed = new MessageEmbed()
+        .setAuthor(member.user.username, member.user.displayAvatarURL({ dynamic: true }))
+        .setColor(color)
+        .setDescription(`**${member.user.tag}** a changé de salon vocal, a quitté **${oldState.channel.name}** et a rejoint **${newState.channel.name}**`)
+        .setTimestamp();
 
-            logChannel.send(embed)
-                .catch(err => console.error('Error sending message:', err));
-        } else {
-            console.log('Log channel not found.');
-        }
+      logChannel.send(embed)
+        .catch(err => console.error('Error sending message:', err));
     }
+  }
 };
